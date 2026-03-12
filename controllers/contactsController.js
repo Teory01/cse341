a4518e24de7a49cff1a2813e897334fc93a55bf0
teory01 <youremail@byupathway.edu>
2026-03-12 11:21:49 +0100

    modified and created a contact json file

--- controllers/contactsController.js
+++ controllers/contactsController.js
@@ -2,10 +2,11 @@ const johnRoute = (req, res) => {
   res.json([
     {
       id: 1,
-      name: 'John Doe',
+      firstName: 'John',
+      lastName: 'Doe',
       email: 'john@email.com',
-      website: 'https://example.com',
-      image: 'base64-image-string'
+      favoriteColor: 'Blue',
+      birthday: '1990-05-15'
     }
   ]);
 };
@@ -14,15 +15,59 @@ const janeRoute = (req, res) => {
   res.json([
     {
       id: 2,
-      name: 'Jane Doe',
-      email: 'jane@email.com',  
-      website: 'https://example.com',
-      image: 'base64-image-string'
+      firstName: 'Jane',
+      lastName: 'Doe',
+      email: 'jane@email.com',
+      favoriteColor: 'Purple',
+      birthday: '1992-08-21'
+    }
+  ]);
+};
+
+const mikeRoute = (req, res) => {
+  res.json([
+    {
+      id: 3,
+      firstName: 'Mike',
+      lastName: 'Johnson',
+      email: 'mike@email.com',
+      favoriteColor: 'Green',
+      birthday: '1988-11-03'
+    }
+  ]);
+};
+
+const sarahRoute = (req, res) => {
+  res.json([
+    {
+      id: 4,
+      firstName: 'Sarah',
+      lastName: 'Williams',
+      email: 'sarah@email.com',
+      favoriteColor: 'Red',
+      birthday: '1995-02-14'
+    }
+  ]);
+};
+
+const davidRoute = (req, res) => {
+  res.json([
+    {
+      id: 5,
+      firstName: 'David',
+      lastName: 'Brown',
+      email: 'david@email.com',
+      favoriteColor: 'Orange',
+      birthday: '1991-09-30'
     }
   ]);
 };
 
 module.exports = {
   johnRoute,
-  janeRoute
+  janeRoute,
+  mikeRoute,
+  sarahRoute,
+  davidRoute
 };
+
--- routes/contacts.js
+++ routes/contacts.js
@@ -3,5 +3,11 @@ const contactsController = require('../controllers/contactsController');
 
 routes.get('/', contactsController.johnRoute);
 routes.get('/JaneDoe', contactsController.janeRoute);
+routes.get('/MikeJohnson', contactsController.mikeRoute);
+routes.get('/SarahWilliams', contactsController.sarahRoute);
+routes.get('/DavidBrown', contactsController.davidRoute);
+
+
+
 
 module.exports = routes; //exporting the routes
