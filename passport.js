8c2f07856509edf78219ce477ae265abd7a3a077
teory01 <youremail@byupathway.edu>
2026-04-06 12:56:54 +0100

    modified

--- passport.js
+++ passport.js
@@ -2,12 +2,14 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 require('dotenv').config();
 
+console.log("Callback URI is:", process.env.CALLBACK_URI);
+
 passport.use(
   new GoogleStrategy(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: process.env.CALLBACK_URL, 
+      callbackURI: process.env.CALLBACK_URI, 
     },
     (accessToken, refreshToken, profile, done) => done(null, profile)
   )
